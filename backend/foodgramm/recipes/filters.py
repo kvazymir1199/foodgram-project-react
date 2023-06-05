1e97d76a4ca8baccf84624e10dc783a76c58e64e
Denis Mironov <denis-mironov1199@yandex.ru>
2023-06-06 01:36:56 +0300

    Update filters.py

--- backend/foodgramm/recipes/filters.py
+++ backend/foodgramm/recipes/filters.py
@@ -1,11 +1,9 @@
 from distutils.util import strtobool
 
 from django_filters import rest_framework
-
 from tags.models import Tag
 
 from .models import FavoriteRecipe, Recipe, ShopingCard
-
 
 CHOICES_LIST = (
     (0, "False"),
