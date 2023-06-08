578fe246f6c9d170999e08c34f984ba3a1664f0e
Denis Mironov <denis-mironov1199@yandex.ru>
2023-06-08 15:55:27 +0300

    Update views.py

--- backend/foodgramm/ingredients/views.py
+++ backend/foodgramm/ingredients/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets,filters
 
 from .models import Ingredient
 from .serializers import IngredientSerializer
@@ -7,3 +7,5 @@ from .serializers import IngredientSerializer
 class IngredientView(viewsets.ModelViewSet):
     serializer_class = IngredientSerializer
     queryset = Ingredient.objects.all()
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('^name',)
